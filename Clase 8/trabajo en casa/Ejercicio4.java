bd7b871eee96909bd4221b5ad7685cc60cee6a60
leandro221p <jlzuniga@estudiante.uniajc.edu.co>
2025-09-30 13:07:01 +0000

    1

--- Clase 8/trabajo en casa/Ejercicio4.java
+++ Clase 8/trabajo en casa/Ejercicio4.java
@@ -1,7 +1,7 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Telefono {
+public class telefono {
     public static void main(String[] args) {
         int[] numeros = {1,2,3,4,5,6,7,8,9,0};
 
